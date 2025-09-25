b5b3fa8fe95a72173d81fd89e019fa8e24a4d6e5
Magesh Kumar A T <it.qa4@nibavlifts.com>
2025-09-25 22:33:45 +0530

    Config issue fixed

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -108,7 +108,7 @@ export default async function handler(req, res) {
 
   try {
     await transporter.sendMail(mailOptions);
-    res.status(200).json({ message: 'Email sent successfully' });
+    res.status(201).json({ message: 'Email sent successfully' });
   } catch (error) {
     console.error('Error sending email:', error);
     res.status(500).json({ message: 'Error sending email' });
